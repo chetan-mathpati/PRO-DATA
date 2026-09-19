36a8f23656db6f0863319cf624d5e066143d2ed4
chetan-mathpati <chetanmathpati4@gmail.com>
2026-09-19 20:42:18 +0530

    fix: preserve null energy risk signals

--- analytics/sql/energy_dependency_signals.sql
+++ analytics/sql/energy_dependency_signals.sql
@@ -12,24 +12,31 @@ WITH base AS (
         crude_share_of_petroleum_production_pct
     FROM analytics.mart_country_economic_energy
 )
+
 SELECT
     *,
+    
     CASE
-        WHEN total_petroleum_production_tbpd IS NOT NULL
-         AND petroleum_production_5yr_avg IS NOT NULL
-         AND total_petroleum_production_tbpd
+        WHEN total_petroleum_production_tbpd IS NULL
+          OR petroleum_production_5yr_avg IS NULL
+            THEN NULL
+        WHEN total_petroleum_production_tbpd
              < petroleum_production_5yr_avg * 0.90
             THEN TRUE
         ELSE FALSE
     END AS production_below_five_year_average,
 
     CASE
+        WHEN petroleum_production_yoy_pct IS NULL
+            THEN NULL
         WHEN petroleum_production_yoy_pct <= -10
             THEN TRUE
         ELSE FALSE
     END AS sharp_production_decline,
 
     CASE
+        WHEN crude_share_of_petroleum_production_pct IS NULL
+            THEN NULL
         WHEN crude_share_of_petroleum_production_pct >= 75
             THEN TRUE
         ELSE FALSE
